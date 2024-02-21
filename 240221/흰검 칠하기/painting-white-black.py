f7521ff75406be473ad55c7fcc1925d0e5949e29
jungtaelim <118165651+jungtaelim@users.noreply.github.com>
2024-02-21 15:07:45 +0900

    Title: 흰검 칠하기, Time: 176ms, Memory: 30MB, Status: Passed - CodeTree

--- 240221/흰검 칠하기/painting-white-black.py
+++ 240221/흰검 칠하기/painting-white-black.py
@@ -1,35 +1,44 @@
-N = int(input())
+MAX_K = 100000
+# 변수 선언 및 입력:
+n = int(input())
+a = [0] * (2 * MAX_K + 1)
+cnt_b = [0] * (2 * MAX_K + 1)
+cnt_w = [0] * (2 * MAX_K + 1)
+b, w, g = 0, 0, 0
 
-M = [[0 for i in range(200002)] for j in range(2)]
+cur = MAX_K
+for _ in range(n):
+    x, c = tuple(input().split())
+    x = int(x)
 
-x = 100001
+    if c == 'L':
+        # x칸 왼쪽으로 칠합니다.
+        while x > 0:
+            a[cur] = 1
+            cnt_w[cur] += 1
+            x -= 1
 
-for i in range(N):
-    a, b = input().split()
-    a = int(a)
-    for j in range(a):
-        if b == 'R':
-            M[0][x] = 1
-            M[1][x] += 1
-            x = x + 1
-        elif b == 'L':
-            M[0][x] = -1
-            M[1][x] += 1
-            x = x - 1
-    if b == 'R':
-        x = x - 1
-    elif b == 'L':
-        x = x + 1
+            if x: 
+                cur -= 1
+    else:
+        # x칸 오른쪽으로 칠합니다.
+        while x > 0:
+            a[cur] = 2
+            cnt_b[cur] += 1
+            x -= 1
 
-B = 0
-W = 0
-G = 0
-for i in range(200002):
-    if M[1][i] >= 4:
-        G += 1
-    elif M[0][i] == 1:
-        B += 1
-    elif M[0][i] == -1:
-        W += 1
+            if x: 
+                cur += 1
 
-print(W, B, G)
+for i in range(2 * MAX_K + 1):
+    # 검은색과 흰색으로 두 번 이상 칠해진 타일은 회색입니다.
+    if cnt_b[i] >= 2 and cnt_w[i] >= 2: 
+        g += 1
+    # 그렇지 않으면 현재 칠해진 색깔이 곧 타일의 색깔입니다.
+    elif a[i] == 1: 
+        w += 1
+    elif a[i] == 2: 
+        b += 1
+
+# 정답을 출력합니다.
+print(w, b, g)
