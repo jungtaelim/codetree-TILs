d5ee122739703cd4c1dda95faddea8da00435b9f
jungtaelim <118165651+jungtaelim@users.noreply.github.com>
2024-02-22 17:55:30 +0900

    Title: 연속되는 수 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240222/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,24 @@
+N = int(input())
+max1 = 0
+be = -1
+count = 0
+for i in range(N):
+    cur = int(input())
+    if i == 0:
+        be = cur
+        count += 1
+        continue
+
+    if be == cur:
+        count += 1
+        be = cur
+        continue
+
+    else:
+        if count > max1:
+            max1 = count
+            count = 1
+            be = cur
+            continue
+
+print(max1)
