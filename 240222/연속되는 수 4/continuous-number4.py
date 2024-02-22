7af8c461529c0f96a22346e5ba32b70676da4a1d
jungtaelim <118165651+jungtaelim@users.noreply.github.com>
2024-02-22 18:15:52 +0900

    Title: 연속되는 수 4, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240222/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,20 @@
+N = int(input())
+
+count = 0
+max1 = 0
+for i in range(N):
+    cur = int(input())
+    if i == 0:
+        count += 1
+    
+    elif be < cur:
+        count += 1
+        if max1 < count:
+            max1 = count
+
+    else:
+        count = 1
+
+    be = cur
+    
+print(max1)
