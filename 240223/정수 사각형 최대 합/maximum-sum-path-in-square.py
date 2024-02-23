8504e241647fe6a9d40dda56c4abdffd6c54d959
jungtaelim <118165651+jungtaelim@users.noreply.github.com>
2024-02-23 16:32:34 +0900

    Title: 정수 사각형 최대 합, Time: 5072ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

---
+++ 240223/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+N = int(input())
+M = [[0 for i in range(N)]for j in range(N)]
+V = [[0 for i in range(N)]for j in range(N)]
+
+for i in range(N):
+    M[i] = list(map(int, input().split()))
+
+
+V[0][0] = M[0][0]
+q = deque()
+q.appendleft((0,0))
+
+dir = [(1,0), (0,1)]
+while q:
+    x,y = q.popleft()
+    for (a,b) in dir:
+        if 0<=x+a<N and 0<= y+b < N:
+            q.appendleft((x+a, y+b))
+            if V[x+a][y+b] < V[x][y]+M[x+a][y+b]: 
+                V[x+a][y+b] = V[x][y]+M[x+a][y+b]
+
+print(V[N-1][N-1])
