e5a64d819d7118fb6b1f985429329eddbcf4e19e
jungtaelim <118165651+jungtaelim@users.noreply.github.com>
2024-02-23 17:52:52 +0900

    Title: 정수 사각형 최대 합, Time: 5049ms, Memory: 26MB, Status: Time Limit Exceed - CodeTree

--- 240223/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 240223/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -1,24 +1,25 @@
 from collections import deque
 
 N = int(input())
-M = [[0 for i in range(N)]for j in range(N)]
-V = [[0 for i in range(N)]for j in range(N)]
+M = [[0 for i in range(N)] for j in range(N)]
+V = [[0 for i in range(N)] for j in range(N)]
 
 for i in range(N):
     M[i] = list(map(int, input().split()))
 
-
 V[0][0] = M[0][0]
 q = deque()
-q.appendleft((0,0))
+q.appendleft((0, 0))
 
-dir = [(1,0), (0,1)]
+dir1 = [(1, 0), (0, 1)]
+
 while q:
-    x,y = q.popleft()
-    for (a,b) in dir:
-        if 0<=x+a<N and 0<= y+b < N:
-            q.appendleft((x+a, y+b))
-            if V[x+a][y+b] < V[x][y]+M[x+a][y+b]: 
-                V[x+a][y+b] = V[x][y]+M[x+a][y+b]
+    x, y = q.popleft()
 
-print(V[N-1][N-1])
+    for (a, b) in dir1:
+        if 0 <= x + a < N and 0 <= y + b < N:
+            if V[x + a][y + b] < V[x][y] + M[x + a][y + b]:
+                V[x + a][y + b] = V[x][y] + M[x + a][y + b]
+                q.appendleft((x + a, y + b))
+
+print(V[N - 1][N - 1])
