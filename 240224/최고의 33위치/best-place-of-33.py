e695c440ac8a83ef9ab41f4466c384686b24601a
jungtaelim <118165651+jungtaelim@users.noreply.github.com>
2024-02-24 17:02:46 +0900

    Title: 최고의 33위치, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240224/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,19 @@
+N = int(input())
+
+M = [[0 for i in range(N)] for j in range(N)]
+
+for i in range(N):
+    M[i] = list(map(int, input().split()))
+
+max1 = 0
+for i in range(N):
+    for j in range(N):
+        if i+2<N and j+2<N:
+            sum1 = 0
+            for k in range(3):
+                for w in range(3):
+                    sum1 = sum1 + M[k][w]
+            if sum1 > max1:
+                max1 = sum1
+
+print(max1)
