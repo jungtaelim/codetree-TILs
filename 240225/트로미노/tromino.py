5ab3e4c1dc7306c21cf58ad365f796a87e53e2c9
jungtaelim <118165651+jungtaelim@users.noreply.github.com>
2024-02-25 17:52:54 +0900

    Title: 트로미노, Time: 209ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240225/트로미노/tromino.py
@@ -0,0 +1,27 @@
+R,C = map(int, input().split())
+
+M = [[0 for i in range(C)] for j in range(R)]
+
+for i in range(R):
+        M[i] = list(map(int, input().split()))
+
+
+max1 = 0
+for i in range(R):
+    for j in range(C):
+        if i+1<R and j+1<C:
+            sum1 = 0
+            sum1 = sum1 + M[i][j]+ M[i+1][j]+ M[i][j+1]+M[i+1][j+1] - min(M[i][j], M[i+1][j], M[i][j+1], M[i+1][j+1])
+            max1 = max(max1, sum1)
+
+        if i+2<R-1:
+            sum1 = 0
+            max1 = max(max1, M[i][j] + M[i+1][j] + M[i+2][j])
+        
+        if j+2<C-1:
+            sum1 = 0
+            max1 = max(max1, M[i][j] + M[i][j+1] + M[i][j+2])
+
+
+
+print(max1)
