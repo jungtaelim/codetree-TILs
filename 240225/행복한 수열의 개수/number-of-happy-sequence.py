4fa538fea7c70a0ea37103f3fdb5f1cb9ae34c33
jungtaelim <118165651+jungtaelim@users.noreply.github.com>
2024-02-25 17:08:50 +0900

    Title: 행복한 수열의 개수, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240225/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240225/행복한 수열의 개수/number-of-happy-sequence.py
@@ -4,25 +4,29 @@ M = [[0 for i in range(N)] for j in range(N)]
 
 for i in range(N):
     M[i] = list(map(int, input().split()))
+
 result = 0
+
 for i in range(N):
-    for j in range(N):
-        for k in range(2):
-            if j+k+1 <N and M[i][j+k+1] == M[i][j+k]:
-                count += 1
-            else:
-                count = 1
+    count = 1
+    for j in range(N-1):
+        if M[i][j] == M[i][j+1]:
+            count+=1
             if count >= A:
-                count = 1
                 result += 1
                 break
-        for k in range(3):
-            if i+k+1<N and M[i+k][j] == M[i+k+1][j]:
-                count += 1
-            else:
-                count = 1
+
+
+
+for i in range(N):
+    count = 1
+    for j in range(N-1):
+        if M[j][i] == M[j+1][i]:
+            count+=1
             if count >= A:
-                count = 1
                 result += 1
                 break
-print(result/2)
+
+
+
+print(result)
