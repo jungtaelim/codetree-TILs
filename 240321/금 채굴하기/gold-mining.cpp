d92aaf2baf8b507c07883d19eff6d7f451ecfc8e
jungtaelim <118165651+jungtaelim@users.noreply.github.com>
2024-03-21 20:12:39 +0900

    Title: 금 채굴하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240321/금 채굴하기/gold-mining.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+
+using namespace std;
+
+#define max_num 20
+int n, m;
+int M[max_num][max_num];
+
+int fee(int k) {
+    return k * k + (k + 1) * (k + 1);
+}
+
+int how_much(int x, int y, int k) {
+    int gold = 0;
+    for (int row = 0; row < n; row++) {
+        for (int col = 0; col < n; col++) {
+            if (abs(row - x) + abs(col - y) <= k) {
+                gold += M[row][col];
+            }
+        }
+    }
+    return gold;
+}
+
+
+int main() {
+
+
+
+    cin >> n >> m;
+   
+
+    for (int row = 0; row < n; row++) {
+        for (int col = 0; col < n; col++) {
+            cin >> M[row][col];
+        }
+    }
+    int maxi = 0;
+
+    for (int row = 0; row < n; row++) {
+        for (int col = 0; col < n; col++) {
+            for (int k = 0; k < 2 * (n - 1); k++) {
+                int a = how_much(row, col, k);
+
+                if (a*m >= fee(k)) {
+                    maxi = max(maxi, a);
+                }
+            }
+        }
+    }
+
+    cout << maxi;
+
+    return 0;
+}
