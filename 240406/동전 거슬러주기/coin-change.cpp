0a4d980872df75431b94466b37a179fc04523430
jungtaelim <118165651+jungtaelim@users.noreply.github.com>
2024-04-06 23:31:40 +0900

    Title: 동전 거슬러주기, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240406/동전 거슬러주기/coin-change.cpp
+++ 240406/동전 거슬러주기/coin-change.cpp
@@ -8,7 +8,7 @@ int main() {
 	int N, M;
 	cin >> N >> M;
 	for (int i = 0; i < 100; i++) {
-		B[i]=INT_MAX;
+		B[i]=100000;
 	}
 	for (int i = 0; i < N; i++) {
 		int a;
@@ -20,13 +20,14 @@ int main() {
 	for (int i = 1; i < M; i++) {
 		for (int j = 0; j < N; j++) {
 
-			if (B[i] != INT_MAX && (B[i + A[j]] > B[i] + 1)) {
+			if (B[i] != 100000 && (B[i + A[j]] > B[i] + 1)) {
 				
 				B[i + A[j]] = B[i];
 				B[i + A[j]]++;
 			}
 		}
 	}
+
 
 	cout << B[M];
 
