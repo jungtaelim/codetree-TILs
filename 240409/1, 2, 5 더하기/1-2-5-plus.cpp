9442da5b68ee5a6ddeee6b127429ad228df539a9
jungtaelim <118165651+jungtaelim@users.noreply.github.com>
2024-04-09 15:15:31 +0900

    Title: 1, 2, 5 더하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240409/1, 2, 5 더하기/1-2-5-plus.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int dp[1000];
+
+int main() {
+	int s[3] = { 1,2,5 };
+	int n;
+	dp[1] = 1;
+	dp[2] = 1;
+	dp[5] = 1;
+	
+	cin >> n;
+	
+	for (int i = 1; i <= n; i++) {
+
+		if (dp[i] != 0) {
+
+			for (int j = 0; j < 3; j++) {
+				if (i + s[j] > n) {
+					continue;
+				}
+				dp[i + s[j]] = dp[i + s[j]] + dp[i];
+
+			}
+		}
+	}
+	
+	cout << dp[n];
+
+}
